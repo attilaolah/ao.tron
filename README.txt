f71dd24a81f365326612dc25f2d7e21a8e8cacc5
aatiis <aatiis@aiur.(none)>
2010-02-13 02:51:59 +0100

    added readme, changelog, and some classifiers

---
+++ README.txt
@@ -0,0 +1,5 @@
+yatron - yet another tron board
+===============================
+
+``yatron`` is a Python module for working with tron boards. For more
+information, and examples, take a look at the documented tests.
--- setup.py
+++ setup.py
@@ -2,18 +2,38 @@ from setuptools import find_packages, setup
 
 
 setup(
-    # Package information
+    # Package information:
     name='yatron',
     version='1.0.0',
     license='GNU GPL',
     url='http://github.com/aatiis/python-yatron',
     description='Tron board and utilities.',
-    # Author information
+    long_description='%s\n\n%s'%(
+        open('README.txt').read(),
+        open('CHANGES.txt').read(),
+    ),
+    # Author information:
     author='Attila Olah',
     author_email='attilaolah@gmail.com',
-    # Package settings
+    # Package settings:
     package_dir={'': 'src'},
     packages=find_packages('src'),
     include_package_data=True,
     zip_safe=True,
+    # Classifiers:
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Buildout',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Topic :: Education',
+        'Topic :: Games/Entertainment',
+        'Topic :: Games/Entertainment :: Simulation',
+    ),
 )
